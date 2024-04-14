52b1c8502cfe1baba9dc4cc519733ce2b7e21acd
Jack Lee <dongjunlee222@gmail.com>
2024-04-14 13:05:26 +0900

    Time: 24 ms (79.85%) | Memory: 15.7 MB (99.10%) - LeetSync

---
+++ 383-ransom-note/ransom-note.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        var copy = magazine
+
+        for letter in ransomNote {
+            if let index = copy.firstIndex(of: letter) {
+                copy.remove(at: index)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+}
