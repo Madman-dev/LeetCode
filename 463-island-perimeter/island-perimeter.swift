c26cdd054493be76e710453ac66854bb2c7819a7
Jack Lee <dongjunlee222@gmail.com>
2024-04-19 00:16:12 +0900

    Time: 144 ms (34.38%) | Memory: 15.8 MB (34.38%) - LeetSync

--- 463-island-perimeter/island-perimeter.swift
+++ 463-island-perimeter/island-perimeter.swift
@@ -1,20 +1,22 @@
 class Solution {
     func islandPerimeter(_ grid: [[Int]]) -> Int {
-        let height = grid.count
-        let length = grid[0].count
-
         var parameter: Int = 0
+        var height = grid.count - 1
+        var length = grid[0].count - 1
 
-        for x in 0..<height {
-            for y in 0..<length where grid[x][y] == 1 {
-                parameter += 4
+        for island in 0...height {
+            for land in 0...length {
+                if grid[island][land] == 1{
+                    parameter += 4
 
-                if x > 0 && grid[x - 1][y] == 1 {
-                    parameter -= 2
-                }
+                    // island and land value HAS to be bigger than 0 to check for adjacent space
+                    if island > 0 && grid[island-1][land] == 1{
+                        parameter -= 2
+                    }
 
-                if y > 0 && grid[x][y-1] == 1 {
-                    parameter -= 2
+                    if land > 0 && grid[island][land-1] == 1{
+                        parameter -= 2
+                    }
                 }
             }
         }
