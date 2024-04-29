f3f3e4166c7405b30b261f3f86712b357126f6df
Jack Lee <dongjunlee222@gmail.com>
2024-04-29 13:41:40 +0900

    Time: 30 ms (9.49%) | Memory: 16.7 MB (8.01%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.swift
+++ 13-roman-to-integer/roman-to-integer.swift
@@ -7,24 +7,18 @@ class Solution {
         var prev = 0
 
         for value in s {
-            // creating non-optional value
             let nonOptional = dict[value] ?? 0
 
-            // check if value is smaller than previous
             if nonOptional <= prev {
                 result += prev
                 print(result)
             
-            // check if not
             } else {
                 result -= prev
-                print("output:", result) // 0 -> -1
             }
             prev = nonOptional
-            print("이전 값:", prev) // 1 -> 5
         }
         result += prev
-        print("출력 이전 output:", result) // 4
         return result
     }
 }
