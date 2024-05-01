545f4e4b31a1365eafbae39756634a49ba43c0d2
Jack Lee <dongjunlee222@gmail.com>
2024-05-01 14:26:05 +0900

    Time: 0 ms (100.00%) | Memory: 15.5 MB (43.71%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.swift
+++ 1635-number-of-good-pairs/number-of-good-pairs.swift
@@ -1,15 +1,12 @@
 class Solution {
     func numIdenticalPairs(_ nums: [Int]) -> Int {
-        // get the index for each pair
-        // check if pair
-        // return good
-
         var count: Int = 0
         
         // index 범위를 넘지 않기 위함
         for num in 0..<nums.count {
-            // 본인 값은 제외할 수 있도록?
+            // 본인 값을 제외하기 위해 num보다 적게 적용
             for value in 0..<num {
+                // 본인 index를 제외한 값끼리 비교, pair 검색
                 if nums[num] == nums[value] {
                     count += 1
                 }
