4f17f7d4d72cc904140168c9ca4364a4c76b323d
Jack Lee <dongjunlee222@gmail.com>
2024-05-07 11:03:53 +0900

    Time: 0 ms (100.00%) | Memory: 15.2 MB (87.10%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.swift
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.swift
@@ -1,13 +1,5 @@
 class Solution {
     func numberOfEmployeesWhoMetTarget(_ hours: [Int], _ target: Int) -> Int {
-        // 확인하는 counter
-        var counter: Int = 0
-        // for문으로 재귀 확인
-        for hour in hours { 
-            if hour >= target {
-                counter += 1
-            }
-        }
-        return counter
+        return hours.filter {$0 >= target}.count
     }
 }
