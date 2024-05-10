3bfb5de86bedd68be1c45ad2f78777fbc5d82366
Jack Lee <dongjunlee222@gmail.com>
2024-05-10 11:40:22 +0900

    Time: 5 ms (31.15%) | Memory: 16 MB (86.89%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.swift
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.swift
@@ -1,6 +1,23 @@
 class Solution {
     func defangIPaddr(_ address: String) -> String {
-        let newIp = address.replacingOccurrences(of: ".", with: "[.]")
-        return newIp
+        // 새로운 배열 생성
+        var result = [String]()
+
+        // 각 Character 값 확인해서 배열에 삽입(at back)
+        for char in address {
+            result.append(String(char))
+        }
+
+        // 결과 배열의 index, 값 형식으로 변환, 정리
+        for (index, char) in result.enumerated() {
+            if char == "." {
+                // 해당 값을 index에서 제거
+                result.remove(at: index)
+                // 해당 위치에 대체 값 input
+                result.insert("[.]", at: index)
+            }
+        }
+
+        return result.joined()
     }
 }
