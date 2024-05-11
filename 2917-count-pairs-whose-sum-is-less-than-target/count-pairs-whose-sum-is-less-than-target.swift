f54ff11fad120ee57d9d54aadad108c71b9cd5df
Jack Lee <dongjunlee222@gmail.com>
2024-05-11 14:30:52 +0900

    Time: 5 ms (83.97%) | Memory: 15.7 MB (17.56%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.swift
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.swift
@@ -2,9 +2,9 @@ class Solution {
     func countPairs(_ nums: [Int], _ target: Int) -> Int {
 
         var counter: Int = 0
-        for i in 0..<nums.count {
-            for j in 0..<i {
-                if nums[i] + nums[j] < target {
+        for (index, num) in nums.enumerated() {
+            for j in 0..<index {
+                if nums[index] + nums[j] < target {
                     counter+=1
                 }
             }
